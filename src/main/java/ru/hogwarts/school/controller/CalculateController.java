d6af34af9fc7e8382eef305398bdd54805044a59
iRUNCHIKK <btkv_irina@icloud.com>
2025-03-16 21:15:32 +0500

    Completed all tasks

--- src/main/java/ru/hogwarts/school/controller/CalculateController.java
+++ src/main/java/ru/hogwarts/school/controller/CalculateController.java
@@ -2,9 +2,12 @@ package ru.hogwarts.school.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.hogwarts.school.model.CalculationResult;
+import ru.hogwarts.school.service.impl.CalculateServiceImpl;
 
 import java.util.stream.Stream;
 
@@ -14,40 +17,15 @@ public class CalculateController {
 
     private final Logger LOGGER = LoggerFactory.getLogger(CalculateController.class);
 
-    @GetMapping
-    public void calculate() {
+    private CalculateServiceImpl calculateServiceImpl;
 
-        //option 1
-        long startTime = System.currentTimeMillis();
-
-        int sum = Stream.iterate(1, a -> a +1)
-                .limit(1_000_000)
-                .reduce(0, Integer::sum);
-
-        long endTime = System.currentTimeMillis();
-
-        LOGGER.info("Option 1: {} ms", endTime - startTime);
-
-        //option 2
-        startTime = System.currentTimeMillis();
-
-        sum = 0;
-        for (int i = 0; i <= 1_000_000 ; i++) {
-            sum += 1;
-        }
-
-        endTime = System.currentTimeMillis();
-
-        LOGGER.info("Option 2: {} ms", endTime - startTime);
-
-        //option 3
-        startTime = System.currentTimeMillis();
-
-        sum = (1 + 1_000_000) * 5000_000;
-
-        endTime = System.currentTimeMillis();
-
-        LOGGER.info("Option 3: {} ms", endTime - startTime);
+    @Autowired
+    public CalculateController(CalculateServiceImpl calculateServiceImpl) {
+        this.calculateServiceImpl = calculateServiceImpl;
     }
 
+    @GetMapping
+    public CalculationResult calculate() {
+        return calculateServiceImpl.performCalculations();
+    }
 }
