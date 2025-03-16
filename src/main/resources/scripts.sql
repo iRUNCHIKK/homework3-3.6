21c0e7c9a000dc4079821d5192be69071dad0228
iRUNCHIKK <btkv_irina@icloud.com>
2025-03-16 13:35:55 +0500

    Completed all tasks

--- src/main/resources/scripts.sql
+++ src/main/resources/scripts.sql
@@ -1,12 +1,10 @@
-select * from student;
-
 select * from student
 where age between min and max;
 
 select name from student;
 
 select * from student
-where name like '%o%';
+where name like '%о%';
 
 select * from student
 where age < id;
